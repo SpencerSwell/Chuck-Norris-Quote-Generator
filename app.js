948891e3f6e25a194e4081df405cecf51e5a8d5f
CodeforLiberty <sswell@live.com>
2016-12-08 14:48:29 -0500

    Category filter added

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
+'use strict';
+var baseUrl = 'http://api.icndb.com/jokes/random';
+var categoryURL = ''
+
 function getDataFromApi () {
-	$.getJSON('http://api.icndb.com/jokes/random', function (quote) {
+	$.getJSON(baseUrl, function (quote) {
 		renderQuote(quote);
 	})
 }
@@ -10,8 +14,33 @@ function renderQuote (quote) {
 
 }
 
+//event handlers
 
 
-$('.js-quote-generator').on("click", function() {
+
+$('.js-quote-generator').on('click', function() {
 	getDataFromApi();
+})
+
+$('.dropdown-content').on('click', '.Nerdy', function() {
+baseUrl = "http://api.icndb.com/jokes/random";
+baseUrl += '?limitTo=[nerdy]';
+console.log(baseUrl);
+
+
+})
+
+$('.dropdown-content').on('click', '.Explicit', function() {
+baseUrl = "http://api.icndb.com/jokes/random";
+baseUrl+= '?limitTo=[explicit]';
+console.log(baseUrl);
+
+
+})
+
+$('.dropdown-content').on('click', '.NerdyAndExplicit', function() {
+baseUrl = 'http://api.icndb.com/jokes/random';
+console.log(baseUrl);
+
+
 })
