60414878cc21af297d6b8fa953cba33ff8006570
CodeforLiberty <sswell@live.com>
2016-12-09 10:41:53 -0500

    Added styles

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ function getDataFromApi () {
 
 
 function renderQuote (quote) {
-	$('.js-quote-box').html('<q>' + quote.value.joke + '</q>');
+	$('.js-quote-box').html('<q>' + quote.value.joke + '</q>' + '<br />' + '<br />');
 
 }
 
@@ -23,34 +23,34 @@ $('.js-quote-generator').on('click', function() {
 })
 
 $('.dropdown-content').on('click', '.Nerdy', function() {
-baseUrl = "http://api.icndb.com/jokes/random";
-baseUrl += '?limitTo=[nerdy]';
-console.log(baseUrl);
+	baseUrl = "http://api.icndb.com/jokes/random";
+	baseUrl += '?limitTo=[nerdy]';
+	console.log(baseUrl);
 
 
 })
 
 $('.dropdown-content').on('click', '.Explicit', function() {
-baseUrl = "http://api.icndb.com/jokes/random";
-baseUrl+= '?limitTo=[explicit]';
-console.log(baseUrl);
+	baseUrl = "http://api.icndb.com/jokes/random";
+	baseUrl+= '?limitTo=[explicit]';
+	console.log(baseUrl);
 
 
 })
 
 $('.dropdown-content').on('click', '.NerdyAndExplicit', function() {
-baseUrl = 'http://api.icndb.com/jokes/random';
-console.log(baseUrl);
+	baseUrl = 'http://api.icndb.com/jokes/random';
+	console.log(baseUrl);
 
 
 })
 
 $('.nameChange').on('submit', function(event) {
- baseUrl = 'http://api.icndb.com/jokes/random';
-var firstName  = $('.fn').val();
-var lastName = $('.ln').val();
-baseUrl+= `?firstName=${firstName}&lastName=${lastName}`;
-console.log(baseUrl);
-event.preventDefault();
+ 	baseUrl = 'http://api.icndb.com/jokes/random';
+	var firstName  = $('.fn').val();
+	var lastName = $('.ln').val();
+	baseUrl+= `?firstName=${firstName}&lastName=${lastName}`;
+	console.log(baseUrl);
+	event.preventDefault();
 
 });
