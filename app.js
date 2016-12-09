baab37c7cc6b945f362860768196c7418f91d411
johnfkneafsey <johnfkneafsey@gmail.com>
2016-12-09 12:11:36 -0500

    All functionality added
    All functionality added

--- app.js
+++ app.js
@@ -4,14 +4,14 @@ var categoryURL = ''
 var fn;
 var ln;
 function getDataFromApi () {
-	$.getJSON(baseUrl, function (quote) {
-		renderQuote(quote);
-	})
+  $.getJSON(baseUrl, function (quote) {
+    renderQuote(quote);
+  })
 }
 
 
 function renderQuote (quote) {
-	$('.js-quote-box').html('<q>' + quote.value.joke + '</q>' + '<br />' + '<br />');
+  $('.js-quote-box').html('<q>' + quote.value.joke + '</q>' + '<br />' + '<br />')
 
 }
 
@@ -20,52 +20,58 @@ function renderQuote (quote) {
 
 
 $('.js-quote-generator').on('click', function() {
-	getDataFromApi();
+  getDataFromApi();
 })
 
 $('.dropdown-content').on('click', '.Nerdy', function() {
-	baseUrl = "http://api.icndb.com/jokes/random";
-	baseUrl += '?limitTo=[nerdy]';
-	console.log(baseUrl);
+  baseUrl = "http://api.icndb.com/jokes/random";
+  baseUrl += '?limitTo=[nerdy]';
+  console.log(baseUrl);
 if(fn && ln) {
-		baseUrl +=`&firstName=${fn}&lastName=${ln}`;
-		console.log(baseUrl);
-	}
+    baseUrl +=`&firstName=${fn}&lastName=${ln}`;
+    console.log(baseUrl);
+  }
 
 })
 
 $('.dropdown-content').on('click', '.Explicit', function() {
-	baseUrl = "http://api.icndb.com/jokes/random";
-	baseUrl+= '?limitTo=[explicit]';
-	console.log(baseUrl);
-		if(fn && ln) {
-			baseUrl +=`&firstName=${fn}&lastName=${ln}`;
-			console.log(baseUrl);
-		}
+  baseUrl = "http://api.icndb.com/jokes/random";
+  baseUrl+= '?limitTo=[explicit]';
+  console.log(baseUrl);
+    if(fn && ln) {
+      baseUrl +=`&firstName=${fn}&lastName=${ln}`;
+      console.log(baseUrl);
+    }
 
 
 })
 
 $('.dropdown-content').on('click', '.NerdyAndExplicit', function() {
-	baseUrl = 'http://api.icndb.com/jokes/random?limitTo=[nerdy]&limitTo=[explicit]';
-	console.log(baseUrl);
-		if(fn && ln) {
-		baseUrl +=`&firstName=${fn}&lastName=${ln}`;
-		console.log(baseUrl);
-	}
+  baseUrl = 'http://api.icndb.com/jokes/random?limitTo=[nerdy]&limitTo=[explicit]';
+  console.log(baseUrl);
+    if(fn && ln) {
+    baseUrl +=`&firstName=${fn}&lastName=${ln}`;
+    console.log(baseUrl);
+  }
+
 
 
 })
 
 $('.nameChange').on('submit', function(event) {
- 	baseUrl = 'http://api.icndb.com/jokes/random';
-	var firstName  = $('.fn').val();
-	var lastName = $('.ln').val();
-	baseUrl+= `?firstName=${firstName}&lastName=${lastName}`;
-	console.log(baseUrl);
-	 fn = firstName;
-	console.log(fn);
-	 ln = lastName;
-	event.preventDefault();
-
+  baseUrl = 'http://api.icndb.com/jokes/random';
+  var firstName  = $('.fn').val();
+  var lastName = $('.ln').val();
+  baseUrl+= `?firstName=${firstName}&lastName=${lastName}`;
+  console.log(baseUrl);
+   fn = firstName;
+  console.log(fn);
+   ln = lastName;
+  event.preventDefault();
 });
+
+
+$('.reset').on('click', function () {
+  fn = undefined;
+  ln = undefined;
+})
